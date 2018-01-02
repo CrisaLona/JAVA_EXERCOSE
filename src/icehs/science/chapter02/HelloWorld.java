44d91328b4a4c6492ef85621faea3aec666b688c
admin <admin@10.137.138.165>
2018-01-02 16:58:02 +0900

    Hello my first programming

---
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -0,0 +1,12 @@
+package icehs.science.chapter02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+			System.out.println("Hello world~~");
+			System.out.println("환영합니다. 여러분!");
+			System.out.println("sys이라 입력하고 Ctrl+space bar를 누르세요.");
+			System.out.println("더블 쿼테이션을 넣고 글을 입력한 뒤에 F11을 눌러보세요.");
+	}
+
+}
