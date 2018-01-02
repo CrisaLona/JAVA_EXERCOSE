3aede29eb797f95dcab47a7297494ba940e3494b
admin <admin@10.137.138.165>
2018-01-02 18:14:07 +0900

    Chater03

---
+++ src/iches/science/chapter03/MyAgeTest.java
@@ -0,0 +1,14 @@
+package iches.science.chapter03;
+
+public class MyAgeTest {
+
+	public static void main(String[] args) {
+		int bornYear;
+		bornYear = 2001;
+		int age = 18;
+		
+		System.out.println(bornYear);
+		System.out.println(age);
+	}
+
+}
---
+++ src/iches/science/chapter03/Variables.java
@@ -0,0 +1,18 @@
+package iches.science.chapter03;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int age= 19;
+		int korScore =95;
+		int engScore=100;
+		int mathScore=88;
+		int totalGoodsPrice=111111;
+		int a=5; //È£¶±ÀÇ ¼ö
+		System.out.println(age);
+		System.out.print(age);
+		System.out.print(age);
+
+	}
+
+}
