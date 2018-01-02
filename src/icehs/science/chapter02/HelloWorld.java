deb828319e1ec7a586ee5fd21c09dd1318ddaa64
admin <admin@10.137.138.165>
2018-01-02 17:10:08 +0900

    WoW

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,10 +1,12 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
+	//My first Java program
 
 	public static void main(String[] args) {
-			System.out.println("Hello world~~");
-			System.out.println("환영합니다. 여러분!");
+//		메인 메서드 안에 주석 달기
+		System.out.println("Hello world~ ~");
+			System.out.println("환영할 겁니다. 여러분!");
 			System.out.println("sys이라 입력하고 Ctrl+space bar를 누르세요.");
 			System.out.println("더블 쿼테이션을 넣고 글을 입력한 뒤에 F11을 눌러보세요.");
 	}
