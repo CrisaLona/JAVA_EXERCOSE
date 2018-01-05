4e8f10ef5e3191f97f06dedd7b08ca61921448cc
admin <admin@10.137.138.165>
2018-01-05 18:25:15 +0900

    Chapter06-Start

---
+++ src/iches/science/chapter06/StudentTest.java
@@ -0,0 +1,20 @@
+package iches.science.chapter06;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student hong = new Student();
+		hong.stuName = "홍길동";
+		hong.stuId = "Stu001";
+		hong.korScore = 94;
+		hong.engScore = 80;
+		hong.mathScore =89;
+
+		hong.printStudentInfo();
+
+		System.out.println("학번 번경");
+		hong.changeStudentId("STU0001");
+		hong.printStudentInfo();
+	}
+
+}
